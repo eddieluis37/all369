185f4804ca398112f856663c3cf482a721d2b990
eddieluis37 <eddierada@gmail.com>
2024-10-13 21:37:38 -0500

    Actualiza logo temporal

--- resources/views/layouts/bt4/welcome.blade.php
+++ resources/views/layouts/bt4/welcome.blade.php
@@ -53,7 +53,7 @@
 
 <body>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm container bg-nav-gobierno">
-        <h5 class="my-0 mr-md-auto font-weight-normal"> <img src="{{ asset('images/gob-header.svg') }}" alt="Logo del gobierno de chile"> </h5>
+        <h5 class="my-0 mr-md-auto font-weight-normal"> <img src="{{ asset('images/cemegs-header.svg') }}" alt="Logo cemegs"> </h5>
         <nav class="my-2 my-md-0 mr-md-3">
             <a class="p-2 text-dark" href="http://www.all369.com">Servicio de Salud</a>
         </nav>
